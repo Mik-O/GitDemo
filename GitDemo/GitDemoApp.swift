122086a8ff5f19126afcc7687d2305b505cbcf5b
Mike <mike@gmail.com>
2021-01-12 02:32:57 +0700

    Initial commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Mishanya on 12.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            print("Hello World!")
+
+        }
+    }
+}
